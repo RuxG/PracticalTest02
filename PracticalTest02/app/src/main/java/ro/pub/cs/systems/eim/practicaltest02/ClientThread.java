6d8529d4437627ba71ea2fc505522dbddf6b1cf3
Ruxandra Grigorie <ruxi.grigorie@gmail.com>
2022-05-30 13:18:22 +0300

    Add sollution

---
+++ PracticalTest02/app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/ClientThread.java
@@ -0,0 +1,73 @@
+package ro.pub.cs.systems.eim.practicaltest02;
+
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ClientThread extends Thread {
+
+    private String address;
+    private int port;
+    private String valuta;
+    private TextView dataTextView;
+
+    private Socket socket;
+
+    public ClientThread(String address, int port, String valuta, TextView dataTextView) {
+        this.address = address;
+        this.port = port;
+        this.valuta = valuta;
+        this.dataTextView = dataTextView;
+    }
+
+    @Override
+    public void run() {
+        try {
+            socket = new Socket(address, port);
+            if (socket == null) {
+                Log.e(Constants.TAG, "[CLIENT THREAD] Could not create socket!");
+                return;
+            }
+            BufferedReader bufferedReader = Utilities.getReader(socket);
+            PrintWriter printWriter = Utilities.getWriter(socket);
+            if (bufferedReader == null || printWriter == null) {
+                Log.e(Constants.TAG, "[CLIENT THREAD] Buffered Reader / Print Writer are null!");
+                return;
+            }
+            printWriter.println(valuta);
+            printWriter.flush();
+
+            String dataInformation;
+            while ((dataInformation = bufferedReader.readLine()) != null) {
+                final String finalizedWeateherInformation = dataInformation;
+                dataTextView.post(new Runnable() {
+                   @Override
+                    public void run() {
+                       dataTextView.setText(finalizedWeateherInformation);
+                   }
+                });
+            }
+        } catch (IOException ioException) {
+            Log.e(Constants.TAG, "[CLIENT THREAD] An exception has occurred: " + ioException.getMessage());
+            if (Constants.DEBUG) {
+                ioException.printStackTrace();
+            }
+        } finally {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException ioException) {
+                    Log.e(Constants.TAG, "[CLIENT THREAD] An exception has occurred: " + ioException.getMessage());
+                    if (Constants.DEBUG) {
+                        ioException.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+}
--- PracticalTest02/app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/PracticalTest02MainActivity.java
+++ PracticalTest02/app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/PracticalTest02MainActivity.java
@@ -3,12 +3,98 @@ package ro.pub.cs.systems.eim.practicaltest02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class PracticalTest02MainActivity extends AppCompatActivity {
+
+    private EditText serverPortEditText = null;
+    private Button connectButton = null;
+
+    // Client widgets
+    private EditText clientAddressEditText = null;
+    private EditText clientPortEditText = null;
+    private EditText informationTypeEditText = null;
+    private Button getDataButton = null;
+    private TextView getDataTextView = null;
+
+    private ServerThread serverThread = null;
+    private ClientThread clientThread = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test02_main);
+
+        serverPortEditText = (EditText)findViewById(R.id.server_port_edit_text);
+
+        connectButton = (Button)findViewById(R.id.connect_button);
+        connectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String serverPort = serverPortEditText.getText().toString();
+                if (serverPort == null || serverPort.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "[MAIN ACTIVITY] Server port should be filled!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                serverThread = new ServerThread(Integer.parseInt(serverPort));
+                if (serverThread.getServerSocket() == null) {
+                    Log.e(Constants.TAG, "[MAIN ACTIVITY] Could not create server thread!");
+                    return;
+                }
+                serverThread.start();
+            }
+        });
+
+        clientAddressEditText = (EditText)findViewById(R.id.client_address_edit_text);
+        clientPortEditText = (EditText)findViewById(R.id.client_port_edit_text);
+        informationTypeEditText = (EditText)findViewById(R.id.valuta);
+
+        getDataButton = (Button)findViewById(R.id.get_data_button);
+        getDataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String clientAddress = clientAddressEditText.getText().toString();
+                String clientPort = clientPortEditText.getText().toString();
+                if (clientAddress == null || clientAddress.isEmpty()
+                        || clientPort == null || clientPort.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "[MAIN ACTIVITY] Client connection parameters should be filled!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (serverThread == null || !serverThread.isAlive()) {
+                    Toast.makeText(getApplicationContext(), "[MAIN ACTIVITY] There is no server to connect to!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                String valuta = informationTypeEditText.getText().toString();
+                if (valuta == null || valuta.isEmpty()  ) {
+                    Toast.makeText(getApplicationContext(), "[MAIN ACTIVITY] Parameters from client valuta should be filled", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                getDataTextView.setText(Constants.EMPTY_STRING);
+
+                clientThread = new ClientThread(
+                        clientAddress, Integer.parseInt(clientPort), valuta, getDataTextView
+                );
+                clientThread.start();
+            }
+        });
+
+        getDataTextView = (TextView)findViewById(R.id.get_data_text_view);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        Log.i(Constants.TAG, "[MAIN ACTIVITY] onDestroy() callback method has been invoked");
+        if (serverThread != null) {
+            serverThread.stopThread();
+        }
+        super.onDestroy();
     }
 }
